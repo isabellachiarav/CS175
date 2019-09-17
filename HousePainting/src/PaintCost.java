36486c493a92ebdd74937de7dd40edb3dcb75404
Isabella Chiaravalloti <s1284564@monmouth.edu>
2019-09-16 20:56:51 -0400

    variables created

---
+++ HousePainting/src/PaintCost.java
@@ -0,0 +1,31 @@
+
+public class PaintCost {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * This program will calculate the cost to paint a house
+		 */
+		double houseLengthFeet;
+		double houseLengthInches;
+		double houseWidthFeet;
+		double houseWidthInches;
+		double houseHeightFeet;
+		double houseHeightInches;
+		double windowLengthFeet;
+		double windowWidthFeet;
+		double doorLengthFeet; 
+		double doorWidthFeet;
+		double sqFtCost;
+		double sqFtPeak;
+		double sqFtNormal;
+		double sqFtWindows; 
+		double sqFtDoors;
+		int windowNumber;
+		int doorNumber;
+		
+		
+
+	}
+
+}
